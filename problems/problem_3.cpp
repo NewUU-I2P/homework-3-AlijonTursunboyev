0e50326ba7085d6a97f74436c2a22da61e7e5ee6
homework 2 <a.tursunboyev@newuu.uz>
2023-11-14 20:48:32 +0500

    solve the problems

--- problems/problem_3.cpp
+++ problems/problem_3.cpp
@@ -1,8 +1,30 @@
 #include <string>
 
 std::string problemSolution3(float height, char S) {
-    // write your code here
 
+    // write your code here
+std::string result;
+if(S=='M'){
+    if(height<1.70)
+        result = "Short";
+    else if (1.70 < height && height <= 1.85)
+            result = "Normal";
+        else if (height >= 1.85)
+            result = "Tall";
+    }else if (S=='F'){
+        if(height<1.60)
+            result="Short";
+        else if(height>1.60 && height<=1.75)
+            result="Normal";
+        else if(height>1.75)
+            result="Tall";
+}
+
+
+
+
+
+return result;
     // use return to return your result
     // make use of control flow statements
 }
--- problems/problem_4.cpp
+++ problems/problem_4.cpp
@@ -3,6 +3,36 @@
 
 std::string problemSolution4(const std::string &macAddress) {
     // write your code here
+    std::string macAddressA = macAddress.substr(0,2);
+    std::string hexadecimal_letters[6]={"A","B","C","D","E","F"};
+    std::string hexadecimal_integer[6]={"10","11","12","13","14","15"};
+    std::string num;
+    std::string result;
+    int temp=0;
+
+    for (int cout=1; cout != -1; count--){
+        num=macAddressA[cout];
+        if(int(num[0])>=65 & int(num[0]<=70)){
+            for (int i=0;i<7;i++){
+                if(hexadecimal_letters[i]==num){
+                    num=hexadecimal_integer[i];
+                    break;
+                }
+            }
+        }
+        temp=temp+(std::stoi(num))*pow(16,2-count-1);
+    }
+    if(temp==255){
+        result="broadcast";
+    } else{
+        if(temp%2 ==1){
+            result="multicast";
+
+        } else{
+            result="unicast";
+        }
+    }
+
 
     // make use of control flow statements
     // return result;
